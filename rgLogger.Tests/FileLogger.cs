a41b7ae780713d8aec0a1cc78826e5a94d44f2c6
Mike D. <mike@eondream.com>
2016-01-31 23:39:31 +0800

    added a unit test project

---
+++ rgLogger.Tests/FileLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rgLogger.Tests {
+    [TestClass]
+    public class FileLogger {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
