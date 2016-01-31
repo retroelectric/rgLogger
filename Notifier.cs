611952bd9894d98a64d1fed194e152f1a09e3270
Mike D. <mike@eondream.com>
2016-01-31 11:22:06 +0800

    started building notification classes added MIT license

---
+++ Notifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgLogger {
+    /// <summary>
+    /// Uses an EmailLogger or CumulativeEmailLogger to send notifications which can be configured to suppress duplicate alerts for a given period of time.
+    /// </summary>
+    public class Notifier {
+        private EmailLogger logWriter;
+        private Dictionary<string, List<string>> notifications;
+
+
+        public Notifier(EmailLogger logger) {
+            logWriter = logger;
+        }
+
+        public string NotificationStorageFile { get; set; } = "rgnotify.dat";
+
+
+    }
+}
