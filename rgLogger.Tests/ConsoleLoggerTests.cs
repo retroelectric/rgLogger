a86b0a00538bb41d2ec3a66dfa32c32753043919
Mike D. <mike@eondream.com>
2016-02-12 05:46:05 +0800

    still working on tests

---
+++ rgLogger.Tests/ConsoleLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rgLogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rgLogger.Tests {
+    [TestClass()]
+    public class ConsoleLoggerTests {
+        [TestMethod()]
+        public void ConsoleLoggerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConsoleLoggerTest1() {
+            Assert.Fail();
+        }
+    }
+}
