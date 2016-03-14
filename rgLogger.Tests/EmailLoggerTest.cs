9c5269df010623c7e4645723b41dde5d4e165886
Mike Denton <michael.denton@kineticit.com.au>
2016-03-14 09:24:02 +0800

    added test for ReplyTo

--- rgLogger.Tests/EmailLoggerTest.cs
+++ rgLogger.Tests/EmailLoggerTest.cs
@@ -7,11 +7,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.QualityTools.Testing.Fakes;
 
 /* Things to check:
- * 1. An email is sent for each call to Write()
+ * 1. [DONE] An email is sent for each call to Write().
  * 2. Messages are not sent at the wrong logging level (test log level filtering)
- * 3. Proxy is configured correctly by the EmailLogger class
- * 4. Async is used when the property is set
- * 5. Adding recipients works correctly
+ * 3. Proxy is configured correctly by the EmailLogger class.
+ * 4. Async is used when the property is set.
+ * 5. Adding recipients works correctly.
+ * 6. ReplyTo uses the Sender address when not set.
  */
 
 using rgLogger;
@@ -36,22 +37,32 @@ namespace rgLogger.Tests {
                     };
                 };
 
-                var logWriter = new EmailLogger("test.server", LogLevel.All) {
+                using (var logWriter = new EmailLogger("test.server", LogLevel.All) {
                     Sender = new MailAddress("fakes@test.server"),
                     Asynchronous = false
-                };
-                logWriter.AddRecipient("recipient@test.server");
+                }) {
+                    logWriter.AddRecipient("recipient@test.server");
 
-                logWriter.Write("the quick brown fox jumped over the lazy dog.", LogLevel.All);
-                logWriter.Write("Who are you people? Torchwood.", LogLevel.Warn);
+                    logWriter.Write("the quick brown fox jumped over the lazy dog.", LogLevel.All);
+                    logWriter.Write("Who are you people? Torchwood.", LogLevel.Warn);
 
-                var expectedResult = new List<string>() {
-                    // log line for logLevel.All output
-                    $"{ new DateTime(1999, 12, 31, 23, 0, 13).ToString(logWriter.TimestampFormat) }  the quick brown fox jumped over the lazy dog.",
-                    $"{ new DateTime(1999, 12, 31, 23, 1, 13).ToString(logWriter.TimestampFormat) } [WARN] Who are you people? Torchwood.",
-                };
+                    var expectedResult = new List<string>() {
+                        // log line for logLevel.All output
+                        $"{ new DateTime(1999, 12, 31, 23, 0, 13).ToString(logWriter.TimestampFormat) }  the quick brown fox jumped over the lazy dog.",
+                        $"{ new DateTime(1999, 12, 31, 23, 1, 13).ToString(logWriter.TimestampFormat) } [WARN] Who are you people? Torchwood.",
+                    };
 
-                CollectionAssert.AreEqual(logMessages, expectedResult);
+                    CollectionAssert.AreEqual(logMessages, expectedResult);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ReplyToUsesSenderAddressWhenNotSet() {
+            using (var logWriter = new EmailLogger("test.server", LogLevel.Warn)) {
+                logWriter.Sender = new MailAddress("sender@fakes.test");
+
+                Assert.AreEqual("sender@fakes.test", logWriter.ReplyTo.Address);
             }
         }
     }
