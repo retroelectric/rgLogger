9a2ecea2b14fb94b6b1ac3958ee6cf3b0438e2b1
Mike Denton <michael.denton@kineticit.com.au>
2016-03-14 11:29:55 +0800

    added additional tests for EmailLogger

--- rgLogger.Tests/EmailLoggerTest.cs
+++ rgLogger.Tests/EmailLoggerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Fakes;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
@@ -9,10 +10,9 @@ using Microsoft.QualityTools.Testing.Fakes;
 /* Things to check:
  * 1. [DONE] An email is sent for each call to Write().
  * 2. Messages are not sent at the wrong logging level (test log level filtering)
- * 3. Proxy is configured correctly by the EmailLogger class.
- * 4. Async is used when the property is set.
- * 5. Adding recipients works correctly.
- * 6. ReplyTo uses the Sender address when not set.
+ * 3. Async is used when the property is set.
+ * 4. Adding recipients works correctly.
+ * 5. [DONE] ReplyTo uses the Sender address when not set.
  */
 
 using rgLogger;
@@ -25,9 +25,9 @@ namespace rgLogger.Tests {
             var logMessages = new List<string>();
 
             using (ShimsContext.Create()) {
-                System.Fakes.ShimDateTime.NowGet = () => {
+                ShimDateTime.NowGet = () => {
                     // make DateTime.Now deterministic based on the number of log messages that have been written.
-                    return new DateTime(1999, 12, 31, 23, logMessages.Count, 13);
+                    return new DateTime(1999, 12, 24, 23, logMessages.Count, 13);
                 };
 
                 ShimSmtpClient.Constructor = @this => {
@@ -48,11 +48,69 @@ namespace rgLogger.Tests {
 
                     var expectedResult = new List<string>() {
                         // log line for logLevel.All output
-                        $"{ new DateTime(1999, 12, 31, 23, 0, 13).ToString(logWriter.TimestampFormat) }  the quick brown fox jumped over the lazy dog.",
-                        $"{ new DateTime(1999, 12, 31, 23, 1, 13).ToString(logWriter.TimestampFormat) } [WARN] Who are you people? Torchwood.",
+                        $"{ new DateTime(1999, 12, 24, 23, 0, 13).ToString(logWriter.TimestampFormat) }  the quick brown fox jumped over the lazy dog.",
+                        $"{ new DateTime(1999, 12, 24, 23, 1, 13).ToString(logWriter.TimestampFormat) } [WARN] Who are you people? Torchwood.",
                     };
 
-                    CollectionAssert.AreEqual(logMessages, expectedResult);
+                    CollectionAssert.AreEqual(logMessages, expectedResult, "Log messages do not match.");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void MessagesAreNotSentAtTheWrongLoggingLevel() {
+            var logMessages = new List<string>();
+
+            using (ShimsContext.Create()) {
+                ShimDateTime.NowGet = () => {
+                    // make DateTime.Now deterministic based on the number of log messages that have been written.
+                    return new DateTime(1999, 12, 24, 23, logMessages.Count, 13);
+                };
+
+                ShimSmtpClient.Constructor = @this => {
+                    var shim = new ShimSmtpClient(@this);
+                    shim.SendMailMessage = e => {
+                        logMessages.Add(e.Body);
+                    };
+                };
+
+                var messagesToSend = new List<KeyValuePair<LogLevel, string>>() {
+                    new KeyValuePair<LogLevel, string>(LogLevel.Debug, "this is a debug message"),
+                    new KeyValuePair<LogLevel, string>(LogLevel.Fatal, "this is a fatal message"),
+                    new KeyValuePair<LogLevel, string>(LogLevel.Info, "this is an info message"),
+                    new KeyValuePair<LogLevel, string>(LogLevel.None, "this is a none message"),
+                    new KeyValuePair<LogLevel, string>(LogLevel.Warn, "this is a warn message"),
+                    new KeyValuePair<LogLevel, string>(LogLevel.All, "this is an all message")
+                };
+
+                foreach (LogLevel lvl in Enum.GetValues(typeof(LogLevel))) {
+                    // reset log messages
+                    logMessages = new List<string>();
+
+                    using (var logWriter = new EmailLogger("test.server", LogLevel.All) {
+                        Sender = new MailAddress("fakes@test.server"),
+                        Asynchronous = false,
+                        Level = lvl
+                    }) {
+                        // write log messages
+                        foreach(var m in messagesToSend.OrderBy(x => x.Key)) {
+                            logWriter.Write(m.Value, m.Key);
+                        }
+
+                        // check log messages
+                        var expectedResults = new List<string>();
+                        int i = 0;
+                        foreach(var m in messagesToSend.OrderBy(x => x.Key)) {
+                            if (lvl != LogLevel.None && m.Key != LogLevel.None) {
+                                if (lvl == LogLevel.All || m.Key <= lvl) {
+                                    expectedResults.Add($"{ new DateTime(1999, 12, 24, 23, i, 13).ToString(logWriter.TimestampFormat) } { ((m.Key == LogLevel.All) ? string.Empty : $"[{ m.Key.ToString().ToUpper() }]") } { m.Value }");
+                                    i++;
+                                }
+                            }
+                        }
+
+                        CollectionAssert.AreEqual(logMessages, expectedResults, $"Failed for log level { lvl.ToString().ToUpper() }.");
+                    }
                 }
             }
         }
@@ -62,7 +120,7 @@ namespace rgLogger.Tests {
             using (var logWriter = new EmailLogger("test.server", LogLevel.Warn)) {
                 logWriter.Sender = new MailAddress("sender@fakes.test");
 
-                Assert.AreEqual("sender@fakes.test", logWriter.ReplyTo.Address);
+                Assert.AreEqual(logWriter.Sender.Address, logWriter.ReplyTo.Address, "ReplyTo does not equal Sender.");
             }
         }
     }
